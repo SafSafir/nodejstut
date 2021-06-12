c32ccb34a0502b971700f788eee41135ac6617d5
Ziya <47672872+SafSafir@users.noreply.github.com>
2021-06-12 15:46:08 +0200

    test2

--- deneme.js
+++ deneme.js
@@ -1,37 +1,9 @@
-const fs = require("fs");
-const util = require("util");
+const EventEmitter = require("events");
 
-readFielPromise = util.promisify(fs.readFile);
-writeFilePromise = util.promisify(fs.writeFile);
+const customEmitter = new EventEmitter();
 
-const start = async () => {
-  try {
-    const first = await readFielPromise("./content/first.txt", "utf8");
-    const second = await readFielPromise("./content/second.txt", "utf8");
-    await writeFilePromise("content/result.txt", `${first} , ${second}`);
-    console.log(first, second);
-  } catch (error) {
-    console.log(error);
-  }
-};
-start();
+customEmitter.on("response", () => {
+  console.log("data received");
+});
 
-// const getText = (path) => {
-//   return new Promise((resolve, reject) => {
-//     fs.readFile(path, "utf8", (err, data) => {
-//       if (err) {
-//         reject(err);
-//       } else {
-//         resolve(data);
-//       }
-//     });
-//   });
-// };
-
-// getText("./first.txt")
-//   .then((result) => {
-//     console.log(result);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
+customEmitter.emit("response");
